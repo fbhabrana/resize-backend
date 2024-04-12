e6355e22e8bcb54acdf95c1ce5c7f34b03013e32
Fakhruddin <fbhabrana@gmail.com>
2024-04-12 21:05:04 +0530

    Fouth Commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Allow requests from localhost:3000
-app.use(cors({ origin: 'https://resize-frontend-two.vercel.app' }));
+app.use(cors());
 
 // Middleware
 app.use(express.json());
